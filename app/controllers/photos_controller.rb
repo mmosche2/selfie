49a4850645737fbeef7db3484750c7578f7c5668
Mark Moschel <mark.moschel@gmail.com>
2014-02-19 23:11:58 -0600

    commit

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -6,7 +6,7 @@ class PhotosController < ApplicationController
   end
 
   def create
-    # @photo = Photo.create( params[:photo] )
+    @photo = Photo.create(user_params)
     @photo.image = params[:image] if params[:image].present?
     @photo.save if @photo.valid?
   end
@@ -14,10 +14,12 @@ class PhotosController < ApplicationController
 # http://stackoverflow.com/questions/10160286/uploading-a-file-to-rails-json-api-server-with-paperclip-and-multipart-request
 # https://github.com/thoughtbot/paperclip/
 
-  # private
+  private
 
-  # def user_params
-  #   params.require(:photos).permit(:image)
-  # end
+  def user_params
+    params.require(:photo).permit(:image)
+  end
 
 end
+
+# curl -v -H "Accept: application/json" -H "Content-type: application/json" -X POST -d ' {"photo":{"image":"fb_photo.jpg"}}'  http://localhost:3000/users/1/photos
